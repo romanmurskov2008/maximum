90f4f4bf9c66b764e6b862f1628d6209ddbd6fba
romanmurskov2008 <blnm5lih@gmail.com>
2023-08-14 18:08:52 +0300

    если честно,то я пользовался интернетом при создании программы, как я понял, она таким образом сравнивает буквы слова

---
+++ PALINDROM.py
@@ -0,0 +1,15 @@
+word = str(input())
+x = len(word)
+i = 0
+x = x - 1
+k = 0
+while x - i >= i:
+    if word[x - i] == word[i]:
+        i += 1
+    else:
+        k = 1
+        break
+if k == 1:
+  print("нет")
+else:
+  print("да")
